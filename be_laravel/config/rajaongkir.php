4f744a2a6334afb4a83148fe472f5f81531c6538
dimszky <schooldims@gmail.com>
2026-06-03 21:03:41 +0700

    clear

--- be_laravel/config/rajaongkir.php
+++ be_laravel/config/rajaongkir.php
@@ -5,3 +5,5 @@ return [
     'base_url'              => env('RO_BASE_URL', 'https://rajaongkir.komerce.id/api/v1'),
     'origin_subdistrict_id' => env('RO_ORIGIN_SUBDISTRICT_ID'), // contoh: 501
 ];
+
+//tes
