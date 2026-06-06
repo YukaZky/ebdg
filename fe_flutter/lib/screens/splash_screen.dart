9e45fecb9d82a8207a1da06b5036a08678310d8c
dimszky <schooldims@gmail.com>
2026-06-06 18:16:48 +0700

    model splash 3

--- fe_flutter/lib/screens/splash_screen.dart
+++ fe_flutter/lib/screens/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'main_screen.dart'; // Karena berada dalam folder yang sama (screens/)
+import 'main_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,8 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    
-    // Mengatur delay selama 3 detik sebelum pindah halaman
+
     Future.delayed(const Duration(seconds: 3), () {
       if (mounted) {
         Navigator.pushReplacement(
@@ -27,21 +26,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Background diubah menjadi putih
-      backgroundColor: Colors.white, 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Menampilkan logo aplikasi yang Anda unggah
-            Image.asset(
-              'assets/logoapk.png',
-              width: 160,
-              height: 160,
-              fit: BoxFit.contain,
-            ),
-            // Loading indicator dan SizedBox di bawahnya sudah dihapus
-          ],
+      body: SizedBox.expand(
+        child: Image.asset(
+          'assets/logoapk.png',
+          fit: BoxFit.cover, // Mengisi seluruh layar tanpa gepeng
         ),
       ),
     );
