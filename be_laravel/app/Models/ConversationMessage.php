77fce129bcce1713415d60f850665beaaf4e6d8c
Dimas Rizky <schooldims@gmail.com>
2026-06-22 20:34:03 +0700

    Add chat item relationships

--- be_laravel/app/Models/ConversationMessage.php
+++ be_laravel/app/Models/ConversationMessage.php
@@ -8,4 +8,14 @@ class ConversationMessage extends Model
 {
     protected $table = 'chat_messages';
     protected $guarded = [];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class, 'conversation_id');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
 }
