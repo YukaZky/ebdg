fc4c5a1e0e6e095e3ce32dd80f420ab1891a943a
Dimas Rizky <schooldims@gmail.com>
2026-06-22 20:33:24 +0700

    Add conversation model relations

--- be_laravel/app/Models/Conversation.php
+++ be_laravel/app/Models/Conversation.php
@@ -8,4 +8,19 @@ class Conversation extends Model
 {
     protected $table = 'chat_conversations';
     protected $guarded = [];
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    public function merchant()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
