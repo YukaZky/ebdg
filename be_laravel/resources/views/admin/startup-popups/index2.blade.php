ba81db8d51654ff7cea23ea10c7eca330ab4f46d
Dimas Rizky <schooldims@gmail.com>
2026-07-04 22:57:29 +0700

    Add startup popup index placeholder

---
+++ be_laravel/resources/views/admin/startup-popups/index2.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.admin')
+@section('content')
+<div class="main-content-inner"><div class="main-content-wrap"><div class="wg-box"><h3>Popup Pembuka Aplikasi</h3></div></div></div>
+@endsection
