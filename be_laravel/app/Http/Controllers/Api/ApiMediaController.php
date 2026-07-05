81651a78240f2ae60a85a575828aefe1e7afe018
Dimas Rizky <schooldims@gmail.com>
2026-07-05 09:43:02 +0700

    Serve startup ad images through media route

--- be_laravel/app/Http/Controllers/Api/ApiMediaController.php
+++ be_laravel/app/Http/Controllers/Api/ApiMediaController.php
@@ -16,6 +16,11 @@ class ApiMediaController extends Controller
         return $this->serveUploadImage('uploads/profiles', $filename);
     }
 
+    public function startupAdImage(string $filename)
+    {
+        return $this->serveUploadImage('uploads/startup-ads', $filename);
+    }
+
     private function serveUploadImage(string $directory, string $filename)
     {
         $safeName = basename($filename);
@@ -32,7 +37,7 @@ class ApiMediaController extends Controller
 
     private function placeholderPng()
     {
-        $png = base64_decode('iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAAAXNSR0IArs4c6QAAAgxJREFUeF7t1LENgDAQRVGf/VcwhOgBkWQF7z3Zc0gq8nmmw1o7M9k/7QH8Au0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAv0Dx1HCOsttfQwAAAAASUVORK5CYII=');
+        $png = base64_decode('iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAAAXNSR0IArs4c6QAAAgxJREFUeF7t1LENgDAQRVGf/VcwhOgBkWQF7z3Zc0gq8nmmw1o7M9k/7QH8Au0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAm0C7QJtAu0CbQLtAu0Dx1HCOsttfQwAAAAASUVORK5CYII=');
 
         return response($png, 200, [
             'Content-Type' => 'image/png',
