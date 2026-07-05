c006ee464ec55fff612247480777d7f2c03dfd4e
Dimas Rizky <schooldims@gmail.com>
2026-07-05 10:13:09 +0700

    Fallback missing about logo uploads

--- be_laravel/bootstrap/app.php
+++ be_laravel/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,6 +11,19 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::get('/uploads/about/{filename}', function (string $filename) {
+                $path = public_path('uploads/about/' . basename($filename));
+
+                if (is_file($path)) {
+                    return response()->file($path, [
+                        'Cache-Control' => 'public, max-age=31536000',
+                    ]);
+                }
+
+                return redirect(asset('assets/images/logo.png'));
+            })->where('filename', '.*');
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
@@ -22,4 +36,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-    
+     
