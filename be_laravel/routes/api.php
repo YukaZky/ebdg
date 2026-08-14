8bc9c3dc187efe1d22236dbad1e6e28a7b8f5191
Dimas Rizky <schooldims@gmail.com>
2026-08-14 11:26:07 +0700

    feat: add protected payout proof route

--- be_laravel/routes/api.php
+++ be_laravel/routes/api.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\Api\ApiUserProfileController;
 use App\Http\Controllers\Api\ApiMediaController;
 use App\Http\Controllers\Api\ApiProductVariationImageController;
 use App\Http\Controllers\Api\ApiStoreIncomeController;
+use App\Http\Controllers\Api\ApiStorePayoutProofController;
 use App\Http\Controllers\MidtransController;
 use App\Http\Controllers\Api\ApiPaymentMethodController;
 
@@ -67,6 +68,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/marketplace/income', [ApiStoreIncomeController::class, 'sellerIncome']);
     Route::get('/marketplace/income/{date}', [ApiStoreIncomeController::class, 'sellerIncomeByDate']);
     Route::get('/marketplace/payouts', [ApiStoreIncomeController::class, 'sellerPayouts']);
+    Route::get('/marketplace/payouts/{id}/proof', [ApiStorePayoutProofController::class, 'show']);
     Route::get('/marketplace/bank-accounts', [ApiStoreIncomeController::class, 'bankAccounts']);
     Route::put('/marketplace/bank-accounts', [ApiStoreIncomeController::class, 'saveBankAccounts']);
     Route::get('/marketplace/my-reviews', [ApiMarketplaceReviewController::class, 'myReviews']);
