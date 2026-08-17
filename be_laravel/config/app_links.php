f279e1456dd6ed531b604d58b322ccb366d18a38
Dimas Rizky <schooldims@gmail.com>
2026-08-17 18:53:39 +0700

    Configure public APK download path

--- be_laravel/config/app_links.php
+++ be_laravel/config/app_links.php
@@ -26,4 +26,19 @@ return [
         'APP_FALLBACK_DOWNLOAD_URL',
         'https://geodesaconnect.id'
     ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | APK Download dari public/
+    |--------------------------------------------------------------------------
+    |
+    | Path bersifat relatif terhadap folder public Laravel. Contoh jika file
+    | berada di public/GeoDesaConnect.apk maka isi APP_APK_PUBLIC_PATH dengan
+    | GeoDesaConnect.apk. Jika berada di public/downloads/app.apk, isi dengan
+    | downloads/app.apk.
+    |
+    */
+
+    'apk_public_path' => env('APP_APK_PUBLIC_PATH', 'GeoDesaConnect.apk'),
+    'apk_download_name' => env('APP_APK_DOWNLOAD_NAME', 'GeoDesaConnect.apk'),
 ];
