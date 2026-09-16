0503be289a7c07b8fa20be426aa7b827596d0ebd
AmmaarSiraj <ammarsirajnomor1pl@gmail.com>
2026-09-16 19:21:19 +0700

    y

--- fe_flutter/lib/services/api_service.dart
+++ fe_flutter/lib/services/api_service.dart
@@ -9,7 +9,7 @@ import '../models/payment_method_model.dart';
 
 class ApiService {
   // static const String baseUrl = "https://ebdg.sidome.id/api";
-  static const String baseUrl = "https://plaything-comfy-graph.ngrok-free.dev/api";
+  static const String baseUrl = "https://geodesaconnect.id/api";
   static const String _tokenStorageKey = 'auth_access_token';
   static String? _token;
   static bool _sessionRestored = false;
